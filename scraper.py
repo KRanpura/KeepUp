2127b7963e3021cc00a9869302cd7179e6ae0433
Khushi <ranpurakhushi@gmail.com>
2024-03-23 16:30:08 -0400

    Actually deleted

--- scraper.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = str("https://www.learndatasci.com/tutorials/ultimate-guide-web-scraping-w-python-requests-and-beautifulsoup/")
-htmlPage = requests.get(url)
-htmlGuts = BeautifulSoup(htmlPage.text)
-
-for link in htmlGuts.find_all('a'):
-    print(link.get('href'))
